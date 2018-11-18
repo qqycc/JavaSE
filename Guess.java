458043ebeabd8b91ce4168fb2a0f3c6bc94d471d
qqycc <963933327@qq.com>
2018-11-18 19:22:59 +0800

    猜数字游戏（更改）

--- Guess.java
+++ Guess.java
@@ -2,7 +2,7 @@ public class Guess{
 	public static void main(String[] args){
 		GuessNumber guess = new GuessNumber();
 		java.util.Random random = new java.util.Random();
-	    private final int value =random.nextInt(100);
+	    final int value =random.nextInt(100);
 		guess.guessNumber(value);
 	}
 }
@@ -10,11 +10,12 @@ public class Guess{
 class GuessNumber{
 	//生成随机数value
 	public void guessNumber(int value){
+		int count = 0;
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		while(true){
 			System.out.println("请输入一个数字：");
 			int num = scanner.nextInt();
-			if(num>this.value){
+			if(num>value){
 				System.out.println("您输入的数字大了");
 		    }else if(num<value){
 		       	System.out.println("您输入的数字小了");
@@ -22,7 +23,9 @@ class GuessNumber{
 			    System.out.println("恭喜你猜对了");
 			    break;
 		    }
+			count++;
 	    }
+		System.out.println("您猜了"+count+"次");
 	}
 }
 
