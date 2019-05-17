6c0cbec7d2286fa93fe0cebe5fb7930ee9105d39
qqycc <963933327@qq.com>
2019-05-17 22:40:58 +0800

    删除重复元素

---
+++ code/OJ/src/com/qqy/Array/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package com.qqy.Array;
+
+/**
+ * 给定一个排序数组，在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间
+ * Author:qqy
+ */
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if(nums==null){
+            return 0;
+        }
+        int len=1;
+        int index=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]!=nums[i]){
+                nums[index++]=nums[i];
+                len++;
+            }
+        }
+        return len;
+    }
+}
