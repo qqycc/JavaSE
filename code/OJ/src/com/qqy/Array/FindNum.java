76b46445bdab14b154515b83f2f69e98b3ac07cc
qqycc <963933327@qq.com>
2019-05-26 21:42:16 +0800

    出现次数大于等于n/2的数

---
+++ code/OJ/src/com/qqy/Array/FindNum.java
@@ -0,0 +1,35 @@
+package com.qqy.Array;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 每个测试输入包含n个空格分割的n个整数，n不超过100，其中有一个整数出现次数大于等于n/2。
+ * 输出出现次数大于等于n/2的数。
+ *
+ * Author:qqy
+ */
+public class FindNum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<Integer> list=new LinkedList<>();
+        while (in.hasNextInt()) {
+           list.add(in.nextInt());
+        }
+
+        int n = list.size();
+        while (!list.isEmpty()) {
+            Object num = ((LinkedList<Integer>) list).peek();
+            int count = 0;
+            while (list.contains(num)) {
+                list.remove(num);
+                count++;
+            }
+            if (count >= n / 2) {
+                System.out.println(num);
+                break;
+            }
+        }
+    }
+}
