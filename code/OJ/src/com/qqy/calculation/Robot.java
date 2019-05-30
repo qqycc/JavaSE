eb77bfcdbf0775b802dc87e89715c6c82c8c5f26
qqycc <963933327@qq.com>
2019-05-30 18:04:24 +0800

    左上角 -> 右上角

---
+++ code/OJ/src/com/qqy/calculation/Robot.java
@@ -0,0 +1,21 @@
+package com.qqy.calculation;
+
+/**
+ * 有一个X*Y的网格，只能走格点且只能向右或向下走，要从左上角走到右下角。计算机器人有多少种走法。
+ * 给定两个正整数int x，int y，请返回走法数目。保证x+y小于等于12。
+ *
+ * 思路
+ *      向右走：x-1
+ *      向下走：y-1
+ *      两个循环
+ *
+ * Author:qqy
+ */
+public class Robot {
+    public static int countWays(int x, int y) {
+        if(x==0||y==0)return 0;
+        if(x==1||y==1)return 1;
+        return countWays(x-1,y)+countWays(x,y-1);
+    }
+
+}
