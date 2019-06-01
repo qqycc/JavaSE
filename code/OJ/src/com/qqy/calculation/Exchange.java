252f1005286e73c7bd5af197f90059da902f593e
qqycc <963933327@qq.com>
2019-06-01 21:28:39 +0800

    无缓存交换

---
+++ code/OJ/src/com/qqy/calculation/Exchange.java
@@ -0,0 +1,16 @@
+package com.qqy.calculation;
+
+/**
+ * 无缓存交换
+ * 编写一个函数，函数内不使用任何临时变量，直接交换两个数的值。
+ * 给定一个int数组AB，其第零个元素和第一个元素为待交换的值，请返回交换后的数组。
+ * Author:qqy
+ */
+public class Exchange {
+    public int[] exchangeAB(int[] AB) {
+        AB[0]=AB[0]^AB[1];
+        AB[1]=AB[0]^AB[1];
+        AB[0]=AB[0]^AB[1];
+        return AB;
+    }
+}
