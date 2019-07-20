abd2010be4d3757f4d791e90583482f5ba13f31e
qqycc <963933327@qq.com>
2019-07-20 22:34:42 +0800

    最大连续和

--- code/OJ/src/com/qqy/Array/MaxSubArray.java
+++ code/OJ/src/com/qqy/Array/MaxSubArray.java
@@ -1,5 +1,7 @@
 package com.qqy.array;
 
+import java.util.Scanner;
+
 /**
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
  * Author:qqy
@@ -20,7 +22,15 @@ public class MaxSubArray {
     }
 
     public static void main(String[] args) {
-        int[] ar={-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(ar));
+        Scanner in = new Scanner;
+        while (in.hasNextLine()) {
+            int num = Integer.parseInt(in.nextLine());
+            String[] strs = in.nextLine().split(" ");
+            int[] nums = new int[num];
+            for (int i = 0; i < num; i++) {
+                nums[i] = Integer.parseInt(strs[i]);
+            }
+            System.out.println(maxSubArray(nums));
+        }
     }
 }
